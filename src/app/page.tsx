ad46f571b480cc624417e5bdf637c80332eddaef
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-07-16 13:14:45 -0300

    aside-created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import { Home as HomeIcon, Search, Library } from 'lucide-react'
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react'
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
+
         <aside className="p-6 w-60 bg-zinc-950">
+          
           <div className="flex items-center gap-2">
             <div className="w-3 h-3 bg-red-500 rounded-full"></div>
             <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
@@ -33,8 +35,72 @@ export default function Home() {
           </nav>
 
         </aside>
-        <main className="p-6 flex-1">main</main>
+
+        <main className="p-6 flex-1">
+          <div className="flex items-center gap-4">
+            <button className='rounded-full bg-black/40 p-2'>
+              <ChevronLeft />
+            </button>
+            <button className='rounded-full bg-black/40 p-2'>
+              <ChevronRight />
+            </button>
+          </div>
+          <h1 className='font-semibold text-3xl mt-3'>Good Afternoon</h1>
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            
+          <div className="bg-white/10 cursor-pointer group rounded-md p-3 flex items-center justify-between gap-4 overflow-hidden hover:bg-zinc-400">
+                <img src="/assets/capa.jpg" width={120} height={120} className='rounded-md' alt="full fight capa album" />
+                <strong>Wasting Light</strong>
+                <button className='w-12 h-12 flex items-center justify-center rounded-full invisible group-hover:visible bg-green-500 text-'>
+                  <Play />
+                </button>
+            </div>
+
+            <div className="bg-white/10 cursor-pointer group rounded-md p-3 flex items-center justify-between gap-4 overflow-hidden hover:bg-zinc-400">
+                <img src="/assets/capa.jpg" width={120} height={120} className='rounded-md' alt="full fight capa album" />
+                <strong>Wasting Light</strong>
+                <button className='w-12 h-12 flex items-center justify-center rounded-full invisible group-hover:visible bg-green-500 text-'>
+                  <Play />
+                </button>
+            </div>
+
+            <div className="bg-white/10 cursor-pointer group rounded-md p-3 flex items-center justify-between gap-4 overflow-hidden hover:bg-zinc-400">
+                <img src="/assets/capa.jpg" width={120} height={120} className='rounded-md' alt="full fight capa album" />
+                <strong>Wasting Light</strong>
+                <button className='w-12 h-12 flex items-center justify-center rounded-full invisible group-hover:visible bg-green-500 text-'>
+                  <Play />
+                </button>
+            </div>
+
+            <div className="bg-white/10 cursor-pointer group rounded-md p-3 flex items-center justify-between gap-4 overflow-hidden hover:bg-zinc-400">
+                <img src="/assets/capa.jpg" width={120} height={120} className='rounded-md' alt="full fight capa album" />
+                <strong>Wasting Light</strong>
+                <button className='w-12 h-12 flex items-center justify-center rounded-full invisible group-hover:visible bg-green-500 text-'>
+                  <Play />
+                </button>
+            </div>
+
+            <div className="bg-white/10 cursor-pointer group rounded-md p-3 flex items-center justify-between gap-4 overflow-hidden hover:bg-zinc-400">
+                <img src="/assets/capa.jpg" width={120} height={120} className='rounded-md' alt="full fight capa album" />
+                <strong>Wasting Light</strong>
+                <button className='w-12 h-12 flex items-center justify-center rounded-full invisible group-hover:visible bg-green-500 text-'>
+                  <Play />
+                </button>
+            </div>
+
+            <div className="bg-white/10 cursor-pointer group rounded-md p-3 flex items-center justify-between gap-4 overflow-hidden hover:bg-zinc-400">
+                <img src="/assets/capa.jpg" width={120} height={120} className='rounded-md' alt="full fight capa album" />
+                <strong>Wasting Light</strong>
+                <button className='w-12 h-12 flex items-center justify-center rounded-full invisible group-hover:visible bg-green-500 text-'>
+                  <Play />
+                </button>
+            </div>
+            
+          </div>
+        </main>
+
       </div>
+
       <footer className="p-6 bg-zinc-800 border-t border-zinc-700">footer</footer>
     </div>
   )
