84ba2cac527e60810e251246bac4775aa7abdf60
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-07-17 08:22:15 -0300

    created-templates-all

---
+++ src/templates/shared/LayoutApp/page.tsx
@@ -0,0 +1,7 @@
+export default function LayoutApp(){
+    return(
+        <div>
+            <p>Hello from LayoutApp</p>
+        </div>
+    )
+}
---
+++ src/templates/shared/Title/page.tsx
@@ -0,0 +1,5 @@
+export default function Title(){
+    return(
+        <h1 className='text-4xl font-bold text-center'>Welcome to Sporty App</h1>
+    )
+}
